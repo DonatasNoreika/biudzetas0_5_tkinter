d5a8453791c781b4fd48ae7565e0346471c72661
Donatas Noreika <donoras@gmail.com>
2023-07-31 09:00:16 +0300

    Galima pridėti pajamas

--- gui_app.py
+++ gui_app.py
@@ -3,16 +3,18 @@ from modules.biudzetas import Biudzetas
 
 biudzetas = Biudzetas()
 
-# def ivesti():
-#     vardas = vardas_entry.get()
-#     pavarde = pavarde_entry.get()
-#     asmuo = Asmuo(vardas, pavarde)
-#     asmenys.append(asmuo)
-#     irasyti_pkl()
-#     asmenys_listbox.delete(0, tk.END)
-#     asmenys_listbox.insert(tk.END, *asmenys)
-#
-#
+def gui_ivesti_pajamas():
+    suma = float(pajamos_suma_entry.get())
+    siuntejas = pajamos_siuntejas_entry.get()
+    info = pajamos_info_entry.get()
+    biudzetas.prideti_pajamu_irasa(suma, siuntejas, info)
+    pajamos_suma_entry.delete(0, tk.END)
+    pajamos_siuntejas_entry.delete(0, tk.END)
+    pajamos_info_entry.delete(0, tk.END)
+    pajamos_suma_entry.focus()
+    zurnalas_listbox.delete(0, tk.END)
+    zurnalas_listbox.insert(tk.END, *biudzetas.zurnalas)
+
 # def istrinti():
 #     indeksas = asmenys_listbox.curselection()[0]
 #     del asmenys[indeksas]
@@ -30,7 +32,7 @@ pajamos_siuntejas_label = tk.Label(window, text="Siuntėjas")
 pajamos_siuntejas_entry = tk.Entry(window)
 pajamos_info_label = tk.Label(window, text="Info")
 pajamos_info_entry = tk.Entry(window)
-pajamos_ivedimas_button = tk.Button(window, text="Įvesti pajamas")
+pajamos_ivedimas_button = tk.Button(window, text="Įvesti pajamas", command=gui_ivesti_pajamas)
 
 # trinti_button = tk.Button(window, text="Ištrinti", command=istrinti)
 zurnalas_listbox = tk.Listbox(window)
@@ -45,13 +47,5 @@ pajamos_info_label.grid(row=3, column=0)
 pajamos_info_entry.grid(row=3, column=1)
 pajamos_ivedimas_button.grid(row=4, columnspan=2)
 zurnalas_listbox.grid(row=5, columnspan=2)
-
-# vardas_label.grid(row=0, column=0)
-# vardas_entry.grid(row=0, column=1)
-# pavarde_label.grid(row=1, column=0)
-# pavarde_entry.grid(row=1, column=1)
-# ivedimas_button.grid(row=2, column=0)
-# trinti_button.grid(row=2, column=1)
-# asmenys_listbox.grid(row=3, columnspan=2)
 
 window.mainloop()
